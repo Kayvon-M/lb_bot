178dbe6928a3793111b6ce2e510fdbe0f33f29bf
Kayvon-M <105576927+Kayvon-M@users.noreply.github.com>
2023-04-14 14:17:05 -0500

    Initial Commit
    .

---
+++ bin/lb_bot.dart
@@ -0,0 +1,5 @@
+import 'package:lb_bot/lb_bot.dart' as lb_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lb_bot.calculate()}!');
+}
---
+++ test/lb_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:lb_bot/lb_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
