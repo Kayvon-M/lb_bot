28d1cc449f966030aab0833371917659af63aa40
Kayvon-M <105576927+Kayvon-M@users.noreply.github.com>
2023-04-14 15:28:13 -0500

    Set up nyxx
    .

--- bin/lb_bot.dart
+++ bin/lb_bot.dart
@@ -1,5 +1,22 @@
-import 'package:lb_bot/lb_bot.dart' as lb_bot;
+import 'package:lb_bot/secrets/secrets.dart';
+import 'package:nyxx/nyxx.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${lb_bot.calculate()}!');
+void main() {
+  final bot = NyxxFactory.createNyxxWebsocket(secrets['token'],
+      GatewayIntents.allUnprivileged | GatewayIntents.messageContent)
+    ..registerPlugin(Logging()) // Default logging plugin
+    ..registerPlugin(
+        CliIntegration()) // Cli integration for nyxx allows stopping application via SIGTERM and SIGKILl
+    ..registerPlugin(
+        IgnoreExceptions()) // Plugin that handles uncaught exceptions that may occur
+    ..connect();
+
+  // Listen for message events
+  bot.eventsWs.onMessageReceived.listen((event) async {
+    if (event.message.content == "!ping" &&
+        event.message.author.id != bot.self.id &&
+        event.message.channel.id == Snowflake("1096523042248736798")) {
+      await event.message.channel.sendMessage(MessageBuilder.content("Pong!"));
+    }
+  });
 }
