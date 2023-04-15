238fd587b58317be94c7fc2782d7b9c9d1771923
Kayvon-M <105576927+Kayvon-M@users.noreply.github.com>
2023-04-15 01:37:53 -0500

    Elo calculator done

--- bin/lb_bot.dart
+++ bin/lb_bot.dart
@@ -1,3 +1,4 @@
+import 'package:lb_bot/core/utils/elo/elo_calculator.dart';
 import 'package:lb_bot/secrets/secrets.dart';
 import 'package:nyxx/nyxx.dart';
 
@@ -17,6 +18,24 @@ void main() {
         event.message.author.id != bot.self.id &&
         event.message.channel.id == Snowflake("1096523042248736798")) {
       await event.message.channel.sendMessage(MessageBuilder.content("Pong!"));
+    } else if (event.message.content.startsWith("!elo") &&
+        event.message.author.id != bot.self.id &&
+        event.message.channel.id == Snowflake("1096523042248736798")) {
+      final elo = event.message.content.split(" ");
+      if (elo.length == 4) {
+        try {
+          double newRating = EloCalculator.newRatingFromScores(
+              double.parse(elo[1]), double.parse(elo[2]), double.parse(elo[3]));
+          await event.message.channel
+              .sendMessage(MessageBuilder.content("New rating: $newRating"));
+        } catch (e) {
+          await event.message.channel
+              .sendMessage(MessageBuilder.content("Invalid command"));
+        }
+      } else {
+        await event.message.channel
+            .sendMessage(MessageBuilder.content("Invalid command"));
+      }
     }
   });
 }
---
+++ lib/core/utils/elo/elo_calculator.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+class EloCalculator {
+  static const double k = 32.0;
+
+  static double expectedScore(double rating1, double rating2) {
+    return 1.0 / (1.0 + pow(10.0, (rating2 - rating1) / 400.0));
+  }
+
+  static double newRating(
+      double rating, double expectedScore, double actualScore) {
+    return rating + k * (actualScore - expectedScore);
+  }
+
+  static double newRatingFromScores(
+      double rating, double opponentRating, double score) {
+    return newRating(rating, expectedScore(rating, opponentRating), score);
+  }
+}
